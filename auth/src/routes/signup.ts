7e4fd87116f7ed621fca56134637340d62ff85e2
Neven <95269427+Proclus01@users.noreply.github.com>
2023-05-29 13:47:03 -0700

    clean up spacing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,25 +1,25 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 cahracters')
-],
-async (req: Request, res: Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 cahracters"),
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
@@ -27,13 +27,14 @@ async (req: Request, res: Response) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
 
     res.status(201).send(user);
-});
+  }
+);
 
 export { router as signupRouter };
