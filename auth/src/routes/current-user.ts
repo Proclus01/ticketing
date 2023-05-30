a7b054c1dc1d507d47e46383cc7f7300b08c6cf3
Neven <95269427+Proclus01@users.noreply.github.com>
2023-05-30 00:38:17 -0700

    update currentuser route with currentuser middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,27 +4,13 @@
 import express from 'express'; // Importing express for routing
 import jwt from 'jsonwebtoken'; // Importing jsonwebtoken for verifying JWTs
 
+import { currentUser } from '../middlewares/current-user';
+
 const router = express.Router(); // Creating a new router object
 
 // Define a route to get the current user
-router.get('/api/users/currentuser', (req, res) => {
-    // If there is no JWT in the session object, return null for currentUser
-    if (!req.session?.jwt) { // The '?' is a TypeScript optional chaining operator. It means if req.session is undefined, it will return undefined instead of throwing an error
-        return res.send({currentUser: null});
-    }
-
-    try {
-        // If there is a JWT, verify it
-        const payload = jwt.verify(
-            req.session.jwt, // The JWT to verify
-            process.env.JWT_KEY! // The secret key to use for verification
-        );
-        // If the JWT is valid, return the payload as the currentUser
-        res.send({ currentUser: payload });
-    } catch {
-        // If the JWT is not valid, return null for currentUser
-        res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null }); // Send back the currentUser in the response, or null if currentUser is not defined
 });
 
 export { router as currentUserRouter }; // Export the router for use in other modules
