0e8ad227a5e8fbf610a16b92c0e1452b40a981c4
Neven <95269427+Proclus01@users.noreply.github.com>
2023-05-30 00:02:07 -0700

    add validation logic for a signed in user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+// The GOAL of this route handler:
+// To figure out if the user is already logged in by making a request about who the user currently is
+// Our code has no direct access or modification of cookies at this step because that is abstracted away
+import express from 'express'; // Importing express for routing
+import jwt from 'jsonwebtoken'; // Importing jsonwebtoken for verifying JWTs
 
-const router = express.Router();
+const router = express.Router(); // Creating a new router object
 
+// Define a route to get the current user
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+    // If there is no JWT in the session object, return null for currentUser
+    if (!req.session?.jwt) { // The '?' is a TypeScript optional chaining operator. It means if req.session is undefined, it will return undefined instead of throwing an error
+        return res.send({currentUser: null});
+    }
+
+    try {
+        // If there is a JWT, verify it
+        const payload = jwt.verify(
+            req.session.jwt, // The JWT to verify
+            process.env.JWT_KEY! // The secret key to use for verification
+        );
+        // If the JWT is valid, return the payload as the currentUser
+        res.send({ currentUser: payload });
+    } catch {
+        // If the JWT is not valid, return null for currentUser
+        res.send({ currentUser: null });
+    }
 });
 
-export { router as currentUserRouter };
+export { router as currentUserRouter }; // Export the router for use in other modules
