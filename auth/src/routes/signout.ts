a312c8390a96474f754d7133617ccb0e262f6ef3
Neven <95269427+Proclus01@users.noreply.github.com>
2023-05-30 00:10:37 -0700

    complete the sign out logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+// A Signout route will send a header that tells the browser
+// to dump all the information stored in the cookie and JWT
+import express from 'express'; // Importing express for routing
 
-const router = express.Router();
+const router = express.Router(); // Creating a new router object
 
+// Define a route to sign out the user
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there!');
+    req.session = null; // Clear the session object, which will also clear the JWT stored in the session
+
+    res.send({}); // Send an empty response
 });
 
-export { router as signoutRouter };
+export { router as signoutRouter }; // Export the router for use in other modules
