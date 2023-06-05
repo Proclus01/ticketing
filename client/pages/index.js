4c930d6cf6aea65cb983b79162f7ef94c30f59b1
Neven <95269427+Proclus01@users.noreply.github.com>
2023-06-05 12:09:46 -0700

    add routing logic to access ssr and client side routes on signup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const landingPage = () => {
-    return <h1>Landing Page</h1>
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  
+//   axios.get("/api/users/currentuser").catch((err) => {
+//     console.log(err.message);
+//   });
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        // we are on the server
+        // requests should be made to the cluster's namespace
+        const namespace = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
+        const response = await axios.get(namespace, {
+            headers: req.headers
+        });
+        return response.data;
+    } else {
+        // we are on the browser
+        // requests can be made with a base url of ''
+        const response = await axios.get('/api/users/currentuser');
+        return response.data;
+    }
+
+
+    return {};
 }
 
-export default landingPage;
+export default LandingPage;
