eec76cc8365eca99feaa2ec8cec97de6d3cde887
Neven <95269427+Proclus01@users.noreply.github.com>
2023-06-05 13:11:24 -0700

    refactor with reusable api client for landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import BuildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -10,24 +10,10 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-        // we are on the server
-        // requests should be made to the cluster's namespace
-        const namespace = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
-        const response = await axios.get(namespace, {
-            headers: req.headers
-        });
-        return response.data;
-    } else {
-        // we are on the browser
-        // requests can be made with a base url of ''
-        const response = await axios.get('/api/users/currentuser');
-        return response.data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const { data } = await BuildClient(context).get('/api/users/currentuser');
 
-
-    return {};
+    return data;
 }
 
 export default LandingPage;
